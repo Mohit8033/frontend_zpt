bc0f5d4f5235f451e63323fe840fd011af8f1ea0
Mohit <mohityadav8026@gmail.com>
2024-01-16 15:58:37 +0530

    Final commit

---
+++ components/Heading.tsx
@@ -0,0 +1,11 @@
+const Heading = () => {
+    return (
+            <div className="text-3xl font-bold text-blue-500 font-serif">
+            Pick Users
+            </div>
+
+
+    );
+};
+
+export default Heading;
---
+++ constants/index.ts
@@ -0,0 +1,64 @@
+import type { Chip } from "../types";
+
+export const Users: Chip[] = [
+    {
+        id: 1,
+        name: "Anita Devi",
+        email: "anita@example.com",
+        avatar: "https://api.multiavatar.com/Anita.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+    {
+        id: 2,
+        name: "Sunil Kumar",
+        email: "sunil@example.com",
+        avatar: "https://api.multiavatar.com/Sunil.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+    {
+        id: 3,
+        name: "Santosh Bai",
+        email: "santosh@example.com",
+        avatar: "https://api.multiavatar.com/Santosh.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+    {
+        id: 4,
+        name: "Sanjay Verma",
+        email: "sanjay@example.com",
+        avatar: "https://api.multiavatar.com/Sanjay.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+    {
+        id: 5,
+        name: "Suman Rastogi",
+        email: "suman@example.com",
+        avatar: "https://api.multiavatar.com/Suman.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+    {
+        id: 6,
+        name: "Shanti Kaur",
+        email: "shanti@example.com",
+        avatar: "https://api.multiavatar.com/Shanti.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+    {
+        id: 7,
+        name: "Ashok Jangra",
+        email: "ashok@example.com",
+        avatar: "https://api.multiavatar.com/Ashok.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+    {
+        id: 8,
+        name: "Lalita Mandal",
+        email: "lalita@example.com",
+        avatar: "https://api.multiavatar.com/Lalita.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+    {
+        id: 9,
+        name: "Umesh Yadav",
+        email: "umesh@example.com",
+        avatar: "https://api.multiavatar.com/Umesh.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+    {
+        id: 10,
+        name: "Deepak Sharma",
+        email: "deepak@example.com",
+        avatar: "https://api.multiavatar.com/Deepak.svg?apikey=ldvt4bR1cTjk6Q",
+    },
+];
